d0486fa270d79c91d1764ef78c24c61c7ec51910
Sproki <sprokiii@gmx.de>
2025-01-25 23:44:00 +0100

    add components/navbar.html edit index.php (index.html)

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+    <title>Document</title>
+    <link rel="stylesheet" href="global.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <!-- Integration der Navigationsleiste -->
+    <?php include("components/navbar.html"); ?>
+</body>
+</html>
