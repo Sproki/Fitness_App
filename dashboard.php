df9b4fa9a25c5753f859a26166b84fc7dde6b166
Sproki <sprokiii@gmx.de>
2025-01-27 18:41:19 +0100

    Rename files

---
+++ dashboard.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+        <title>Document</title>
+        <link rel="stylesheet" href="global.css">
+        <script src="https://cdn.tailwindcss.com"></script>
+    </head>
+    <header>
+        <?php include("components/profile.html"); ?>
+        <?php include("components/navbar.html"); ?>
+    </header>
+    <body class="">
+        <div class="mx-[40px]">
+            <div class="my-5">
+                <h2 class="text-2xl">Willkommen zurück💪</h2>
+                <h1 class="text-3xl font-bold">Pascal</h1>
+            </div>
+            <div class="grid grid-flow-col grid-rows-2 gap-[10px]">
+                <div class="bg-gray-100 w-[150px] h-[150px] row-span-2 rounded-[15px]"></div>
+                <div class="bg-gray-100 w-[150px] h-[70px] col-span-2 rounded-[15px]">
+                    <div class="grid grid-flow-col grid-rows-2">
+                        <p class="text-center row-span-2">🔥</p>
+                        <p class="text-[12px] col-span-2">Verbrannte Kalorien</p>
+                        <p class="text-[12px] col-span-2">300 kcal</p>
+                    </div>
+                </div>
+                <div class="bg-gray-100 w-[150px] h-[70px] col-span-2 rounded-[15px]"></div>
+            </div>
+        </div>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -1,33 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
-        <title>Document</title>
-        <link rel="stylesheet" href="global.css">
-        <script src="https://cdn.tailwindcss.com"></script>
-    </head>
-    <header>
-        <?php include("components/profile.html"); ?>
-        <?php include("components/navbar.html"); ?>
-    </header>
-    <body>
-        <div class="mx-[40px]">
-            <div class="my-5">
-                <h2 class="text-2xl">Willkommen zurück💪</h2>
-                <h1 class="text-3xl font-bold">Pascal</h1>
-            </div>
-            <div class="grid grid-flow-col grid-rows-2 gap-[10px]">
-                <div class="bg-gray-300 w-[150px] h-[150px] row-span-2 rounded-[15px]"></div>
-                <div class="bg-gray-300 w-[150px] h-[70px] col-span-2 rounded-[15px]">
-                    <div class="grid grid-flow-col grid-rows-2">
-                        <p class="text-center row-span-2">🔥</p>
-                        <p class="text-[12px] col-span-2">Verbrannte Kalorien</p>
-                        <p class="text-[12px] col-span-2">300 kcal</p>
-                    </div>
-                </div>
-                <div class="bg-gray-300 w-[150px] h-[70px] col-span-2 rounded-[15px]"></div>
-            </div>
-        </div>
-    </body>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Anmeldemaske</title>
+    <!-- Link zu deinem externen CSS -->
+    <link rel="stylesheet" href="dashboard.css">
+</head>
+<body>
+<div class="background"></div>
+    <div class="formInputs">
+        <form action="dashboard.php" method="POST">
+            <input class="inputField" type="text" name="vorname" placeholder="Vorname..." required>
+            <input class="inputField" type="text" name="nachname"placeholder="Nachname..." required>
+            <button type="submit" class="button">Anmelden</button>
+        </form>
+    </div>
+    <p class="foundersText">by Pascal, Gabriel & Maurice</p>
+</body>
 </html>
