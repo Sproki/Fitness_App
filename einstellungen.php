cc4494faadb433f6f451841a5faa1d753fa0958e
Moritzmd <141919658+Moritzmd@users.noreply.github.com>
2025-01-30 16:14:15 +0100

    add einstellungen.php

---
+++ einstellungen.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+        <title>Document</title>
+        <link rel="stylesheet" href="global.css">
+    </head>
+
+    <header>
+        <?php include("components/profile.html"); ?>
+        <?php include("components/navbar.html"); ?>
+    </header>
+
+    <body>
+        
+    </body>
+
+    <script src="https://cdn.tailwindcss.com"></script>
+
+    <script src="scripts/darkModeHandler.js"></script>
+    <script src="scripts/tailwind.config.js"></script>
+
+</html>
