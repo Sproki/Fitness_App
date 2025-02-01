39c8a74af9e70285778672e447d603b63f2ab6aa
Sproki <sprokiii@gmx.de>
2025-02-02 00:08:03 +0100

    edit register.php to show conflicts

--- register.php
+++ register.php
@@ -52,7 +52,7 @@
 </head>
 <body class="flex items-center justify-center h-screen">
 <div class="fixed inset-0 bg-cover bg-center bg-no-repeat scale-110 blur-[3px] -z-10" style="background-image: url('img/loginBackgroundImg.png');"></div>
-    <form action="register.php" method="POST" class="flex flex-col gap-4">
+    <form id="registerForm" action="register.php" method="POST" class="flex flex-col gap-4">
         <div class="flex flex-col gap-4">
             <div class="flex flex-col items-center text-center">
                 <h1 class="text-3xl font-bold text-white">Registrieren</h1>
@@ -60,8 +60,10 @@
             </div>
             <input placeholder="Vorname" type="text" name="firstname" class="w-[300px] h-[45px] rounded-full bg-gray-600 p-4 outline-none text-white">
             <input placeholder="Nachname" type="text" name="lastname" class="w-[300px] h-[45px] rounded-full bg-gray-600 p-4 outline-none text-white">
-            <input placeholder="Email" type="email" name="email" class="w-[300px] h-[45px] rounded-full bg-gray-600 p-4 outline-none text-white">
-            <input placeholder="Passwort" type="password" name="password" class="w-[300px] h-[45px] rounded-full bg-gray-600 p-4 outline-none text-white">
+            <input placeholder="Email" type="email" name="email" id="email" class="w-[300px] h-[45px] rounded-full bg-gray-600 p-4 outline-none text-white">
+            <input placeholder="Email wiederholen" type="email" name="confirmEmail" id="confirmEmail" class="w-[300px] h-[45px] rounded-full bg-gray-600 p-4 outline-none text-white">
+            <input placeholder="Passwort" type="password" name="password" id="password" class="w-[300px] h-[45px] rounded-full bg-gray-600 p-4 outline-none text-white">
+            <input placeholder="Passwort wiederholen" type="password" name="confirmPassword" id="confirmPassword" class="w-[300px] h-[45px] rounded-full bg-gray-600 p-4 outline-none text-white">
         </div>
         <button name="submit" class="text-center bg-gray-300 w-[300px] h-[45px] rounded-full text-gray-600">Account Erstellen</button>
         <a href="index.php" class="text-center text-gray-300 hover:text-gray-600">Anmelden</a>
@@ -70,5 +72,39 @@
 </body>
 
 <script src="https://cdn.tailwindcss.com"></script>
+
+<script>
+    document.getElementById("registerForm").addEventListener("submit", function(event) {
+        let email = document.getElementById("email");
+        let confirmEmail = document.getElementById("confirmEmail");
+        let password = document.getElementById("password");
+        let confirmPassword = document.getElementById("confirmPassword");
+
+        let hasError = false;
+
+        // Überprüfung der E-Mail
+        if (email.value !== confirmEmail.value) {
+            email.classList.remove("bg-gray-600");
+            confirmEmail.classList.remove("bg-gray-600");
+            email.classList.add("bg-red-500");
+            confirmEmail.classList.add("bg-red-500");
+            hasError = true;
+        }
+
+        // Überprüfung des Passworts
+        if (password.value !== confirmPassword.value) {
+            password.classList.remove("bg-gray-600");
+            confirmPassword.classList.remove("bg-gray-600");
+            password.classList.add("bg-red-500");
+            confirmPassword.classList.add("bg-red-500");
+            hasError = true;
+        }
+
+        // Falls Fehler vorhanden sind, verhindern wir das Absenden des Formulars
+        if (hasError) {
+            event.preventDefault();
+        }
+    });
+</script>
 
 </html>
